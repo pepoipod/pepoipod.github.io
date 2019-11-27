331593f42b810227164a34e6ff0cd53b821a7695
iwamoto <iwamoto@cshool.jp>
2019-11-27 17:50:17 +0900

    cliを4.x化した際のbuildエラーを修正

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,23 @@
 <template>
-  <div>
-    　　　△　　￥　▲<br />
-    　　（　㊤　皿　㊤）　　がしゃーん<br />
-    　　（　　　　　　　　）　　　　　　<br />
-    　／│　　肉　　│＼　　　　　　　　　がしゃーん<br />
-    ＜　　＼＿＿__／　　＞<br />
-    　　　　┃　　　┃<br />
-    　　　　＝　　　＝<br />
-    pepoipodだよ<br />
-    コードが書けるすごいやつだよ
+  <div style="white-space: pre-wrap;">
+    {{ body }}
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  data: () => ({
+    body: `
+         △   ￥  ▲
+       （  ㊤  皿  ㊤）   がしゃーん
+       （                ）
+      ／│   肉     │＼          がしゃーん
+    ＜   ＼＿＿__／   ＞
+           ┃     ┃
+           ＝      ＝
+    pepoipodだよ
+    コードが書けるすごいやつだよ`
+  })
 };
 </script>
