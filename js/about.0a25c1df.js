8442e08d720b80e8d7b7da8697265887dbd1a160
iwamoto <iwamoto@cshool.jp>
2019-11-27 17:50:35 +0900

    Updates

--- js/about.0a25c1df.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("\n  見せもんじゃないよ、帰んな！！！\n")])},l=[],a=t("2877"),c={},o=Object(a["a"])(c,u,l,!1,null,null,null);e["default"]=o.exports}}]);
-//# sourceMappingURL=about.0a25c1df.js.map
